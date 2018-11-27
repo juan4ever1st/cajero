9ca1a5ace456f7d9b41facdfd75d6bb31371e5b4
opera020 <opera020@WS51L020.ribeiront.net.ar>
2018-11-27 20:41:29 -0300

    ACTUALIZACION 3 27-11-18

--- src/Cajero/CajeroApp.java
+++ src/Cajero/CajeroApp.java
@@ -5,7 +5,7 @@ public class CajeroApp {
     public static void main(String[] args) {
         
         Operaciones opera = new Operaciones(); //creacion del objeto para la manipulacion de los metodos de la clase operaciones        
-        opera.menu();//invocacion del menu por medio del objeto opera
+        opera.menu();//llamamos al menu por medio del objeto opera
         System.exit(0); //cuando se sale del menu se finaliza la aplicacion
     }    
 }
--- src/Cajero/Operaciones.java
+++ src/Cajero/Operaciones.java
@@ -5,17 +5,17 @@ import javax.swing.JOptionPane;//importacion necesaria para el uso de los cuadro
 public class Operaciones {
     
     //variables globales para que puedan ser modificadas de ser necesario
-    int saldo=90000; //saldo inicial
+    int saldo=100000; //saldo inicial
     String clave="1234";//clave predeterminada
     
     public void menu(){//metodo que contiene el menu de la aplicacion
         
-        String ax;
+        String aux;
         int op=0;        
         do{
-            ax = JOptionPane.showInputDialog(null, "1. SALDO \n 2. DEPOSITAR \n 3. RETIRAR \n 4. CAMBIAR CLAVE \n 5. SALIR");
-            if(ax!=null && Validar.isNum(ax)){//validacion para el menu
-                op = Integer.parseInt(ax);
+            aux = JOptionPane.showInputDialog(null, "MENU PRINCIPAL \n\n 1. SALDO \n 2. DEPOSITAR \n 3. RETIRAR \n 4. CAMBIAR CLAVE \n 5. SALIR \n");
+            if(aux!=null && Validar.isNum(aux)){//validacion para el menu
+                op = Integer.parseInt(aux);
                 switch(op){
                     case 1:
                         verSal();
@@ -41,11 +41,11 @@ public class Operaciones {
     }
     
     public void depo(){//metodo para realizar un deposito
-        String ax; int deposito=0;        
+        String aux; int deposito=0;        
         
-        ax = JOptionPane.showInputDialog(null, "Cuanto desea depositar? ");
-        if((ax!=null) && !ax.equals("") && Validar.isNum(ax)){//triple validacion, si se ingreso algo, y si es numero lo ingresado            
-            deposito = Integer.parseInt(ax);
+        aux = JOptionPane.showInputDialog(null, "Cuanto desea depositar? ");
+        if((aux!=null) && !aux.equals("") && Validar.isNum(aux)){//triple validacion, si se ingreso algo, y si es numero lo ingresado            
+            deposito = Integer.parseInt(aux);
             if(deposito > 0){//si el valor a consignar es mayor que 0 se realiza la consignacion
                 saldo+=deposito;//se suma el nuevo valor al saldo
             }else{
@@ -58,48 +58,48 @@ public class Operaciones {
     
     public void retirar(){//metodo para retirar del saldo
         
-        String ax;                
-        ax = JOptionPane.showInputDialog(null, "Digite su clave: ");
+        String aux;                
+        aux = JOptionPane.showInputDialog(null, "Digite su clave: ");
         
-        if(ax==null){//validacion por si se selecciona cancelar del cuadro de dialogo y evitar errores 
+        if(aux==null){//validacion por si se selecciona cancelar del cuadro de dialogo y evitar errores 
             
         }else{
-            if(!ax.equals("")){            
-                if(ax.equals(clave)){//si la clave digitada coincide con la clave
-                    ax = JOptionPane.showInputDialog(null, "A. 500 \n B. 1.000 \n C. 2.000 \n D. 5.000 \n E. 10.000 \n F. INGRESAR MONTO",
+            if(!aux.equals("")){            
+                if(aux.equals(clave)){//si la clave digitada coincide con la clave
+                    aux = JOptionPane.showInputDialog(null, "A. 500 \n B. 1.000 \n C. 2.000 \n D. 5.000 \n E. 10.000 \n F. INGRESAR MONTO",
                             "CANTIDAD A RETIRAR", JOptionPane.INFORMATION_MESSAGE);
-                    if(ax!=null){
-                        if(!ax.equals("")){
-                            switch(ax){
-                                case "a":
+                    if(aux!=null){
+                        if(!aux.equals("")){
+                            switch(aux){
+                                case "A":
                                     if((saldo-500>=10000)){//se verifica si al retirar queda con un saldo minimo de 10.000
                                         saldo-=500;
                                     }                            
                                     break;
-                                case "b":
+                                case "B":
                                    if((saldo-1000>=10000)){//se verifica si al retirar queda con un saldo minimo de 10.000
                                         saldo-=1000;
                                     }
                                     break;
-                                case "c":
+                                case "C":
                                    if((saldo-2000>=10000)){//se verifica si al retirar queda con un saldo minimo de 10.000
                                         saldo-=2000;
                                     }
                                     break;
-                                case "d":
+                                case "D":
                                     if((saldo-5000>=10000)){//se verifica si al retirar queda con un saldo minimo de 10.000
                                         saldo-=5000;
                                     }
                                     break;
-                                case "e":
+                                case "E":
                                     if((saldo-10000>=10000)){//se verifica si al retirar queda con un saldo minimo de 10.000
                                         saldo-=10000;
                                     }
                                     break;
-                                case "f":
-                                    ax = JOptionPane.showInputDialog(null, "Ingrese el monto a retirar: ");
-                                    if(!ax.equals("") && Validar.isNum(ax) && (Integer.parseInt(ax)>0) && (saldo-Integer.parseInt(ax)>=10000) ){
-                                        saldo-= Integer.parseInt(ax);
+                                case "F":
+                                    aux = JOptionPane.showInputDialog(null, "Ingrese el monto a retirar: ");
+                                    if(!aux.equals("") && Validar.isNum(aux) && (Integer.parseInt(aux)>0) && (saldo-Integer.parseInt(aux)>=10000) ){
+                                        saldo-= Integer.parseInt(aux);
                                     }
                                     break;
                                 default:
@@ -118,20 +118,20 @@ public class Operaciones {
    
     public void cambiarPass(){//metodo para cambiar la contraseña
         
-        String ax, cl;
+        String aux, cl;
         
-        ax = JOptionPane.showInputDialog(null, "Digite su clave actual....");
-        if(ax!=null){
-            if(!ax.equals("")){            
-                if(ax.equals(clave)){//si la clave digitada coincide con la clave que existe
+        aux = JOptionPane.showInputDialog(null, "Digite su clave actual....");
+        if(aux!=null){
+            if(!aux.equals("")){            
+                if(aux.equals(clave)){//si la clave digitada coincide con la clave que existe
                    cl = JOptionPane.showInputDialog(null, "Digite su nueva clave");
                    if(cl!=null){
                        if(cl.length()>3){//si el tamaño de la nueva contraseña tiene mas de 3 caracteres se puede cambiar
-                            ax=null;//se elimina el valor contenido con anterioridad de la variable
-                            ax="Su anterior clave es: "+clave;
-                            clave=cl;
-                            ax+="\nSu nueva clave es: "+clave; 
-                            JOptionPane.showMessageDialog(null, ax);
+                            aux = null;//se elimina el valor contenido con anterioridad de la variable
+                            aux = "Su anterior clave es: "+clave;
+                            clave = cl;
+                            aux+= "\nSu nueva clave es: "+clave; 
+                            JOptionPane.showMessageDialog(null, aux);
                        }
                        else{
                             JOptionPane.showMessageDialog(null, "La clave ingresada es muy corta...(Debe contener al menos 4 carateres)");
